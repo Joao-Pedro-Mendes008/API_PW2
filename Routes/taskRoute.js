b63fc87415331f8b0a93e63293a054cc297b9f3e
João Pedro <joaopedromendefernandes08@gmail.com>
2025-06-13 19:55:47 -0300

    atualizações no código na parte de tarefas

--- Routes/taskRoute.js
+++ Routes/taskRoute.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const taskController = require('../Controllers/taskController');
 
-router.post('/users/:id/tasks', taskController.createTask);
-router.post('/users/:id/listTasks', taskController.listTask);
+router.post('/users/:id/createTasks', taskController.createTask);
+router.get('/users/:id/listTasks', taskController.listTask);
 router.post('/users/:id/updateTasks', taskController.updateTask);
 router.post('/users/:id/deleteTasks', taskController.deleteTask);
 
